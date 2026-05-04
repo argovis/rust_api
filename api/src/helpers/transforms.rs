4863fb82e66c8fad9f41ffa266f3bdfc1d77346d
katieannemills <you@example.com>
2026-05-04 18:09:43 -0400

    match db at iter 156

--- api/src/helpers/transforms.rs
+++ api/src/helpers/transforms.rs
@@ -157,7 +157,6 @@ mod tests {
             level: 5.0,
             cell_vertical_fraction: 1.0,
             sea_binary_mask_at_t_locaiton: true,
-            ctrl_vector_3d_mask: true,
             cell_z_size: 1.0,
             reference_density_profile: 1.0,
             data,
